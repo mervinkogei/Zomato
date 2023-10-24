45fcad08a515d0cfc1a8ffadbf6e8757299e9e48
mervinkogei <vincent.kogei@yahoo.com>
2023-10-24 14:15:29 +0300

    initial components categories

---
+++ src/app/pages/restraunt-items/restraunt-items.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-restraunt-items',
+  templateUrl: './restraunt-items.component.html',
+  styleUrls: ['./restraunt-items.component.scss']
+})
+export class RestrauntItemsComponent {
+
+}
